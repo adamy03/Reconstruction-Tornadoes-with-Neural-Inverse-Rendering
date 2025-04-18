95a9b0a2f9a43668b961884f3fb2736699c6ea8f
Nadula Kadawedduwa <53409084+NadulaKadawedduwa@users.noreply.github.com>
2025-04-18 14:16:48 -0400

    Add files via upload

--- pi_code/client.py
+++ pi_code/client.py
@@ -10,10 +10,9 @@ print(f"Recording for {num_seconds} seconds...")
 
 # server_ip = ip_address
 server_port = 12345                 # Same port as the server
-client_sockets = [None] * num_pis
-pi_ids = [1, 2, 3, 6, 7, 8, 9]
+client_sockets = []
 
-for i in pi_ids:
+for i in range(1,num_pis+1):
     hostname = f"rpitest{i}.local"
     while True:
         try:
@@ -22,7 +21,7 @@ for i in pi_ids:
             print(f"Raspberry Pi{i} IP address: {ip_address}")
 
             # Create a socket object
-            client_sockets[i-1] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_sockets.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
 
             # Connect to the server
             client_sockets[i-1].connect((ip_address, server_port))
@@ -35,28 +34,24 @@ for i in pi_ids:
 # Send a message to the server
 message = str(num_seconds)
 
-for j in pi_ids:
+for j in range(num_pis):
     print(f"CMessage sent to rpi{j}")
-    client_sockets[j-1].send(message.encode('utf-8'))
+    client_sockets[j].send(message.encode('utf-8'))
 
-for k in pi_ids:
+for k in range(num_pis):
     # Receive a response from the server
-    print(client_sockets[k-1])
-    with open(f"test_rpi{k}.mp4", 'wb') as f:
+    with open(f"test_rpi{k+1}.mp4", 'wb') as f:
         print("Receiving the file...")
-        try:
-            while True:
-                data = client_sockets[k-1].recv(1024)  # Receive data in chunks of 1024 bytes
-                if not data:
-                    break  # If no data is received, the transfer is complete
-                f.write(data)  # Write the received data to the file
-        except:
-            print("Error receiving data pi '{k}' from the server.")
-            break
+        while True:
+            data = client_sockets[k].recv(1024)  # Receive data in chunks of 1024 bytes
+            if not data:
+                break  # If no data is received, the transfer is complete
+            f.write(data)  # Write the received data to the file
+
 
     # response = client_sockets[k].recv(1024).decode('utf-8')
-    print(f"Received response from rpi{k}")
+    print(f"Received response from rpi{k+1}")
     
     # Close the connection
-    client_sockets[k-1].close()
+    client_sockets[k].close()
 
